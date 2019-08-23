10dc8e06dbebe668d55622a20a743fab95c22a8b
vladigralov <53748671+vladigralov@users.noreply.github.com>
2019-08-23 13:44:56 +0300

    update component with Vue

---
+++ client/registration.vue
@@ -0,0 +1,30 @@
+<style>
+	#web{
+	background-color: red;
+	color: white;
+	border: 2px solid red;
+	width: 500px;
+	}
+
+	input, #web{
+		box-shadow: 0 0 7px rgba(0,0,0,0.5);
+	}
+</style>
+
+
+<template>
+	<div>
+		<form>
+			  <div class="form-group">
+			    <input type="login" class="form-control" id="login" aria-describedby="emailHelp" placeholder="Логин">
+			  </div>
+			  <div class="form-group">
+			    <input type="password" class="form-control" id="password" placeholder="Пароль">
+			  </div>
+			  <div class="form-group">
+			    <input type="password" class="form-control" id="repeat password" placeholder="Повтор пароля">
+			  </div>
+			  <button type="submit" class="btn btn-primary" id="web">Зарегистрироваться!</button>
+		</form>
+	</div>
+</template>
